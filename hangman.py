777bbc664106ef57066e207a0568149015a74d0b
Jake <jakehead96@gmail.com>
2023-09-02 21:56:45 +0100

    Added more functionality

--- hangman.py
+++ hangman.py
@@ -9,7 +9,7 @@ def generate_word():
     with open(filepath, "r") as f:
         countries = f.readlines()
     
-    chosen_country = random.choice(countries).strip("\n")
+    chosen_country = random.choice(countries).strip("\n").upper()
 
     return chosen_country
 
@@ -19,6 +19,7 @@ def print_info(disp, guessed_letters, remaining_letters):
     print(f"{disp}")
     print(f"Guessed letters: {guessed_letters}")
     print(f"Remaining letters {remaining_letters}")
+
 
 def obtain_valid_input(guessed_letters):
     current_letter = ""
@@ -35,43 +36,59 @@ def obtain_valid_input(guessed_letters):
     return current_letter
 
 
-
-    return current_letter
-
-def clear_console():
-    os.system("cls")
-
 def check_winner(guesses):
     
     if guesses == 10:
-        print("You lose!")
+        return False
     else:
-        print("You win!")
+        return True
 
 def main():
 
-    input("Welcome to Hangman, press any key to begin!")
+    print("Welcome to Hangman")
+    print("Spaces in the target word are denoted by a backslash.")
+    input("Press any key to begin!")
     
     target_word = generate_word()
+    guessed_word = "".join(["\\ " if c == " " else "_ " for c in target_word])
 
     remaining_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     guessed_letters = ""
 
-    
     guesses = 0
-    while guesses < 10:
+    while (guesses < 10):
 
         # Print game info
         print_info(display[guesses], guessed_letters, remaining_letters)
+        print(target_word)
+        print(guessed_word)
 
         # Obtain new guess
         current_letter = obtain_valid_input(guessed_letters)
 
-        
+        # Update guessed letters
         guessed_letters += current_letter
         
-        guesses += 1
+        # Update guessed word
+        if current_letter in target_word:
+            letter_indexes = [idx for idx, char in enumerate(target_word) if char == current_letter]
+            print(letter_indexes)
 
+        # Update remaining letters
+        remaining_letters = remaining_letters.replace(current_letter, " ")
+
+        # Check winner
+        if (guessed_word == target_word):
+            break
+
+        guesses += 1
+    
+    # Check for winner
+    if check_winner(guesses):
+        print("You win!")
+    else:
+        print(f"You lose! The target word = {target_word}")
+        print(display[guesses])
 
 
 if __name__ == "__main__":
